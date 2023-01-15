d8d04c9c35c2ac0baa29408e1eb03cb9f7876156
Andreas Hoelzl <andy.woodly@gmail.com>
2023-01-15 20:32:39 +0100

    package

---
+++ finsim/__init__.py
@@ -0,0 +1,9 @@
+"""
+finsim
+
+Toolkit for financial return simulations.
+"""
+
+__version__ = "0.0.1"
+__author__ = 'Andreas Hoelzl'
+__credits__ = 'HEADWALL LLC'
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='finsim',
+    version='0.0.1',
+    description='Tookit for financial simulations',
+    url='https://github.com/headwallc/finsim.git',
+    author='HEADWALL LLC',
+    author_email='dev@headwallc.com',
+    license='proprietary',
+    packages=['finsim'],
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 1 - Beta',
+        'Intended Audience :: Financial Advisory',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+    ]
+)
